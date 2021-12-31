2750a2ddb858e5634b12c4fa225033c7a5aa7155
alan <alanarostegui@hotmail.com>
2021-12-30 23:06:01 -0600

    feat: scaffolding the cart

---
+++ cart/webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederatedWebpackPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFederatedWebpackPlugin({
+      name: "cart",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./CartShow": "./src/index",
+      },
+    }),
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
