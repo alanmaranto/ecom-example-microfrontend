ec4d891c7351ec667a6f872e10d134d83020833b
alan <alanarostegui@hotmail.com>
2022-01-01 14:24:31 -0600

    feat: refactor cart to keep the functionality with the possibles scenarios

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,6 +1,29 @@
-import faker from 'faker';
+import faker from "faker";
 
-const cartText = `<div>You have ${faker.datatype.number()} items in your cart.</div>`;
+const mount = (htmlElement) => {
+  const cartText = `<div>You have ${faker.datatype.number()} items in your cart.</div>`;
 
-document.querySelector('#dev-cart').innerHTML = cartText;
+  htmlElement.innerHTML = cartText;
+};
 
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// which definitely has an element with an id of 'dev-cart'
+// We want to inmediately render our app into that element
+if (process.env.NODE_ENV !== "production") {
+  const htmlEl = document.querySelector("#dev-cart");
+
+  // Assuming our container doesnt have an element
+  if (htmlEl) {
+    // We are probable running in isolation
+    mount(htmlEl);
+  }
+}
+
+// Context/Situation #2
+// We are running this file in development or production
+// through the CONTAINER app
+// and there is NO GUARANTEE that an element with and id of 'dev-cart' exists
+// WE DO NOT WANT TO try to inmediately render the app
+export { mount };
--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
 import { mount } from 'products/ProductsIndex'
-import 'cart/CartShow'
+import {mount as mountCart } from 'cart/CartShow'
 
 console.log('container')
 
 mount(document.querySelector('#my-products'))
+mountCart(document.querySelector('#my-cart'))
